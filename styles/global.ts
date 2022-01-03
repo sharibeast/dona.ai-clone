ba90728467110c423bedc1e9ee8e8aae64098857
Shareef <sharibeast@gmail.com>
2022-01-03 16:56:26 +0300

    fixing font size

--- styles/global.ts
+++ styles/global.ts
@@ -6,6 +6,13 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     outline: 0;
     box-sizing: border-box;
+
+
+}
+
+body{
     font-family: 'Inter', 'sans-serif';
+    line-height: 1.45;
+    font-size: 16px;
 }
 `;
