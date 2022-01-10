292fd039bdc67a4cc0fcea0a7c347fbba232387b
Sharif <sharibeast@gmail.com>
2022-01-10 23:16:07 +0300

    build(about page): its summary and description

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,12 @@ import TodoListShowCase from 'components/TodoListShowCase';
 import Layout from 'components/Layout/Layout';
 import Input from 'components/Input/Input';
 import ShowTabs from 'components/ShowTabs/ShowTabs';
+import Head from 'components/Head/Head';
 
 const Home: NextPage = () => {
   return (
     <Layout>
+      <Head title='Dona - Home' description='a more humane to-do list'/>
       <HeroSection />
       <ShowCase />
       <TodoListShowCase />
