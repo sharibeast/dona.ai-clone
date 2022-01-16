4048e67b3409621336762f4910e17bc80e263748
Sharif <sharibeast@gmail.com>
2022-01-16 17:52:28 +0300

    fix: remove vercel icon

--- components/Head/Head.tsx
+++ components/Head/Head.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import NextHead from 'next/head'
 
 
-interface HeadProps{
-    title:string;
-    description?:string;
+interface HeadProps {
+    title: string;
+    description?: string;
 }
 
-export default function Head({title,description}:HeadProps):JSX.Element {
+export default function Head({ title, description }: HeadProps): JSX.Element {
     return (
         <NextHead>
-        <title>{title}</title>
-        <meta name="description" content={description} />
-        <link rel="icon" href="/favicon.ico" />
-      </NextHead>
+            <title>{title}</title>
+            <meta name="description" content={description} />
+            {/* <link rel="icon" href="/favicon.ico" /> */}
+        </NextHead>
     )
 }
